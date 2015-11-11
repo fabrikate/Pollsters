9d700fc7b17494cfa2a9e52f2598f94887b5538f
Mary Mott <mott.maryt@gmail.com>
2015-11-11 11:50:21 -0800

    fixed migration tables

--- Pollsters/db/migrate/20151111194714_create_users.rb
+++ Pollsters/db/migrate/20151111194714_create_users.rb
@@ -1,10 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.text :email
-      t.text :password
-
-      t.timestamps null: false
+      t.string :email
+      t.string :password
+      t.string :password_digest
     end
   end
 end
