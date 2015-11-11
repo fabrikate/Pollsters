c7f25f8f370e5deb07ebd2d7468f353e92d0b448
KateStearns <katestearns@mac.com>
2015-11-11 15:56:37 -0800

    polls and options crud works

--- Pollsters/app/controllers/options_controller.rb
+++ Pollsters/app/controllers/options_controller.rb
@@ -2,63 +2,35 @@ class OptionsController < ApplicationController
   before_action :set_option, only: [:show, :edit, :update, :destroy]
 
   # GET /options
-  # GET /options.json
   def index
     @options = Option.all
+    render json: @options
   end
 
-  # GET /options/1
-  # GET /options/1.json
   def show
+    render json: @option, status: :ok
   end
 
-  # GET /options/new
-  def new
-    @option = Option.new
-  end
-
-  # GET /options/1/edit
-  def edit
-  end
-
-  # POST /options
-  # POST /options.json
   def create
     @option = Option.new(option_params)
-
-    respond_to do |format|
-      if @option.save
-        format.html { redirect_to @option, notice: 'Option was successfully created.' }
-        format.json { render :show, status: :created, location: @option }
-      else
-        format.html { render :new }
-        format.json { render json: @option.errors, status: :unprocessable_entity }
-      end
+    if @option.save
+      render json: @option, status: :created
+    else
+      render json: @option.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /options/1
-  # PATCH/PUT /options/1.json
   def update
-    respond_to do |format|
-      if @option.update(option_params)
-        format.html { redirect_to @option, notice: 'Option was successfully updated.' }
-        format.json { render :show, status: :ok, location: @option }
-      else
-        format.html { render :edit }
-        format.json { render json: @option.errors, status: :unprocessable_entity }
-      end
+    if @option.update(option_params)
+      render json: @option, status: :ok
+    else
+      render json: @option.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /options/1
-  # DELETE /options/1.json
   def destroy
     @option.destroy
-    respond_to do |format|
-      format.html { redirect_to options_url, notice: 'Option was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    render json: @option, status: :ok
   end
 
   private
--- Pollsters/app/models/user.rb
+++ Pollsters/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: {minimum: 6}, length: {maximum: 20}
+
+  has_many :polls, dependent: :destroy
 end
