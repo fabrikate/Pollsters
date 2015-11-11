e07f08ba6a3d31af52ec82cab50f392b36fd2c3c
Brea Gaudioso <breagaudioso@gmail.com>
2015-11-11 13:38:48 -0800

    commented out scope api routes

--- Pollsters/config/routes.rb
+++ Pollsters/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   
   root to: 'statics#index'
-  scope '/api' do
-    resources :polls, only: [:index, :show, :create, :update, :destroy]
-  end
+  
+  # scope '/api' do
+  #   resources :polls, only: [:index, :show, :create, :update, :destroy]
+  # end
 
 end
