11de1c04e1040ba2377109a1d952a2e916d8e4e1
KateStearns <katestearns@mac.com>
2015-11-13 14:32:44 -0800

    create poll/options WORKS

--- Pollsters/app/assets/javascripts/angular/app/polls/polls.controller.js
+++ Pollsters/app/assets/javascripts/angular/app/polls/polls.controller.js
@@ -10,14 +10,16 @@
   function PollsController(PollFactory, OptionFactory){
     var vm = this;
 
+    // query all polls from the database
     var Polls = PollFactory.get({}, function(data) {
       vm.Polls = data.polls;
     });
-
+    // query all options from the database
     var Options = OptionFactory.get({}, function(data) {
       vm.Options = data.options;
     })
 
+    // form objects
     vm.option = {
       answer: '',
       vote: 0,
@@ -31,20 +33,23 @@
     vm.pollDB = [];
     vm.optionsDB = [];
 
+    //save poll name to the database
     vm.savePollName = function() {
       vm.createdPoll = new PollFactory();
       vm.createdPoll.title = vm.poll.title;
-      PollFactory.save(vm.createdPoll, function() {
-        console.log('saved api call: ', vm.createdPoll);
+      PollFactory.save(vm.createdPoll).$promise.then(function(data) {
+        vm.currentPoll_id = data.poll.id;
       })
-      // $('.pollTitle').hide();
+      $('.pollTitle').hide();
     }
+
     vm.save = function() {
-      vm.option.poll_id = vm.Polls.length;
+      vm.option.poll_id = vm.currentPoll_id;
       vm.optionsDB.push(angular.copy(vm.option));
       vm.option.poll_id = null;
       vm.option.answer = '';
     }
+
     vm.saveToDB = function() {
       vm.optionsDB.forEach(function(item) {
         vm.createdOption = new OptionFactory();
@@ -52,7 +57,6 @@
         vm.createdOption.poll_id = item.poll_id
         vm.createdOption.vote = item.vote
         OptionFactory.save(vm.createdOption, function() {
-          console.log('saved api option ', vm.createdOption);
         })
       })
     }
@@ -60,3 +64,5 @@
 
 })();
 
+// the actual poll id - > where?
+// save that to pol id in options before we push to DB
