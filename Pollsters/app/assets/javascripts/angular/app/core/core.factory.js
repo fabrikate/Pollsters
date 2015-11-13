728f2e01d8d924a05a5406947eb0628dfdd2cc8b
Brea Gaudioso <breagaudioso@gmail.com>
2015-11-13 09:25:31 -0800

    for merge

--- Pollsters/app/assets/javascripts/angular/app/core/core.factory.js
+++ Pollsters/app/assets/javascripts/angular/app/core/core.factory.js
@@ -5,12 +5,12 @@
   .factory('PollFactory', PollFactory);
 
   PollFactory.$inject = ['$resource']
+
   function PollFactory($resource) {
-    var data = $resource('http://localhost:3000/api/polls/:poll', {poll: '@poll'}, {
+   return $resource('http://localhost:3000/api/polls/:poll', {poll: '@poll'}, {
       update: {
         method: 'PUT'
       }
     });
-    return data;
-  }
+  };
 })();
