36be3062ed380a516f58557ba923c847bc17c7c3
KateStearns <katestearns@mac.com>
2015-11-12 19:48:15 -0800

    in process of model update

--- Pollsters/app/assets/javascripts/angular/app/core/core.factory.js
+++ Pollsters/app/assets/javascripts/angular/app/core/core.factory.js
@@ -1,16 +1,16 @@
-(function(){
+ (function(){
   'use strict';
   angular
   .module('app.core')
   .factory('PollFactory', PollFactory);
 
   PollFactory.$inject = ['$resource']
+
   function PollFactory($resource) {
-    var data = $resource('http://localhost:3000/api/polls/:poll', {poll: '@poll'}, {
+   return $resource('http://localhost:3000/api/polls/:poll', {poll: '@poll'}, {
       update: {
         method: 'PUT'
       }
     });
-    return data;
-  }
+  };
 })();
