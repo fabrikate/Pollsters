992520c914928f14580a262b0795c9ea2fabf0bb
Brea Gaudioso <breagaudioso@gmail.com>
2015-11-17 14:22:31 -0800

    user page working '

--- Pollsters/app/assets/javascripts/angular/app/core/core.factory.js
+++ Pollsters/app/assets/javascripts/angular/app/core/core.factory.js
@@ -8,7 +8,7 @@
   PollFactory.$inject = ['$resource'];
 
   function PollFactory($resource) {
-   return $resource('http://localhost:3000/api/polls/:poll', {poll: '@poll'}, {
+   return $resource('http://localhost:3000/api/polls/:id', {id: '@id'}, {
       update: {
         method: 'PUT'
       }
