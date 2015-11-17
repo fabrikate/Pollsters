a14520e174d390f7f80bc8173053d307e607566d
Mary Mott <mott.maryt@gmail.com>
2015-11-16 16:05:47 -0800

    successful progress on message handling for updating user account

--- Pollsters/app/assets/javascripts/angular/app/users/users.controller.js
+++ Pollsters/app/assets/javascripts/angular/app/users/users.controller.js
@@ -10,74 +10,95 @@
   function UsersController($routeParams, UserService, AuthService) {
     var vm = this;
 
-    console.log(AuthService.current);
+    // console.log(AuthService.current);
     vm.current = AuthService.current;
 
-    function getUserInfo() {
-      // console.log($routeParams.user);
+    //get user's info, paramter is boolean to reset updateEmail toggle or not
+    function getUserInfo(reset) {
       vm.user = UserService.get({id: $routeParams.user});
+
+      if (reset) {
+        vm.updateEmail = false;
+        vm.emailErrors = true;
+      }
     }
 
     function toggleEmailShow() {
-      vm.updateEmail = !vm.updateEmail;
+      vm.updateEmail = true;
     }
 
     function togglePwReset() {
-      vm.resetPw = !vm.resetPw;
+      vm.resetPw = true;
     }
 
     function updateUser() {
-      var createResponse;
-// update({id: vm.contact.id},
-//           {
-//             name: vm.contact.name,
-//             email: vm.contact.email,
-//             phone: vm.contact.phone
-//           }
-
-//clear out user shit when updated
-
-//send in per request
+      var updatePasswordResponse;
+      var updateEmailResponse;
+      //send in per request
+      //update email
+      if (vm.updateEmail)      {
+// console.log(vm.user.email, vm.user.password);
+        updateEmailResponse = UserService.update({id: vm.user.id}, {email: vm.user.email});
+        updateEmailResponse.$promise.then(function(data) {
+          console.log(data);
+          // console.log(data.email);
+          // if (Array.isArray(data.email)) {
+              //these are errors!
+          //   vm.emailChanged = 'Email updated successfully.'
+          // }
+          getUserInfo(true);
+        }, function(response) {
+          vm.emailErrors = response.data.email;
+          vm.passwordErrors = response.data.password;
+          getUserInfo(false);
+          console.log(response.data);
+        });
+      }
+      //update password
       if (vm.user.password) {
         if (vm.user.password === vm.confirmPw) {
-        //update entire account
-        // createResponse = UserService.update({id: vm.user.id}, vm.user);
-
+          console.log(vm.user.password, vm.confirmPw);
+          updatePasswordResponse = UserService.update({id: vm.user.id}, {password: vm.user});
         } else {
-          // vm.passwordErrors = ['Passwords do not match.'];
+          //word 'Password' is already in view
+          clearUserPw();
+          return vm.passwordNotMatched = 'Passwords do not match.';
         }
-      } else {
-console.log(vm.user.email, vm.user.password);
-        //update email only
-        createResponse = UserService.update({id: vm.user.id}, {email: vm.user.email});
+        updatePasswordResponse.$promise.then(function(data) {
+          console.log(data);
+          // console.log(data.email);
+          // if (Array.isArray(data.email)) {
+              //these are errors!
+          //   vm.emailChanged = 'Email updated successfully.'
+          // }
+          clearUserPw();
+        }, function(response) {
+          vm.emailErrors = response.data.email;
+          vm.passwordErrors = response.data.password;
+          clearUserPw();
+          console.log(response.data);
+        });
       }
-      createResponse.$promise.then(function(data) {
-        console.log(data);
-        clearUserPw();
-      }, function(response) {
-        vm.emailErrors = response.data.email;
-        vm.passwordErrors = response.data.password;
-        clearUserPw();
-        console.log(response.data);
-      });
     }
-
 
     function clearUserPw() {
       vm.user.password = null;
       vm.confirmPw = null;
     }
 
-    function cancel() {
+    function reset() {
       vm.updateEmail = false;
       vm.resetPw = false;
+      vm.emailErrors = null;
+      vm.passwordErrors = null;
+      vm.passwordNotMatched = null;
     }
 
     vm.toggleEmailShow = toggleEmailShow;
     vm.togglePwReset =  togglePwReset;
     vm.updateUser = updateUser;
-    vm.cancel = cancel;
+    vm.reset = reset;
 
-    getUserInfo();
+    getUserInfo(true);
   }
 })();
