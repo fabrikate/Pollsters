d7e63eb90058af46a1834362a5c5b0afff4843ac
KateStearns <katestearns@mac.com>
2015-11-18 10:24:37 -0800

    vote function on show page

--- Pollsters/app/assets/javascripts/angular/app/polls/polls.showController.js
+++ Pollsters/app/assets/javascripts/angular/app/polls/polls.showController.js
@@ -10,38 +10,40 @@
   function showController ( PollFactory, OptionFactory, $http, $routeParams) {
     var vm = this;
     vm.pollNum = $routeParams.poll_id
-    console.log(vm.pollNum);
-    vm.sum = 0;
-
+    //following 3 functions allow a user to vote on the show page
     vm.upVote = function(){
       var ID = vm.chooseVote;
-      var choice = OptionFactory.get({id:ID}, function(){
-      console.log('pre choice', choice.option)
+      var choice = OptionFactory.get({id:ID}, function() {
         choice.option.vote += 1;
         choice.$update(choice.option);
-        console.log('post choice', choice.option.vote)
-        PollFactory.get({id:vm.pollNum}).$promise.then(function(data){
-          console.log('promise data is ', data.poll)
-          vm.showPoll = data.poll
-        })
+        vm.showFunctionality();
+        vm.sum++;
       });
     };
+    vm.showFunctionality = function() {
+      PollFactory.get({id: vm.pollNum}).$promise.then(function(data) {
+        vm.showPoll = data.poll;
+        vm.pollLabels = [];
+        vm.pollData = [];
 
-    PollFactory.get({id: vm.pollNum}).$promise.then(function(data) {
-      console.log('get request ', data.poll.options);
-      vm.showPoll = data.poll;
-      vm.pollLabels = [];
-      vm.pollData = [];
-
-      vm.showPoll.options.forEach(function(item) {
-        vm.pollLabels.push(item.answer);
-        vm.pollData.push(item.vote)
+        vm.showPoll.options.forEach(function(item) {
+          vm.pollLabels.push(item.answer);
+          vm.pollData.push(item.vote)
+        })
+        console.log('pollData', vm.pollData)
       })
+    }
+    vm.findSum = function() {
+      vm.sum = 0;
       vm.pollData.forEach(function(item) {
+        console.log('tem is, ', item);
         vm.sum += item;
       })
-    })
+    }
+    // variable tied to what graph style is selected.
     vm.selected = '';
-  }
+    vm.showFunctionality();
 
+
+  }
 })();
