8c444880facd396ddeaa5caf79724af5f2e18303
Mary Mott <mott.maryt@gmail.com>
2015-11-17 17:41:11 -0800

    fixed new account session bug

--- Pollsters/app/controllers/users_controller.rb
+++ Pollsters/app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       render json: { id: @user.id, email: @user.email }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
