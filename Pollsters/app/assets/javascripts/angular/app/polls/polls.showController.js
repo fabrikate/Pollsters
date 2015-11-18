898df81186fae6dda942c1f878805cf50eb311b8
KateStearns <katestearns@mac.com>
2015-11-18 09:45:39 -0800

    refactor show page

--- Pollsters/app/assets/javascripts/angular/app/polls/polls.showController.js
+++ Pollsters/app/assets/javascripts/angular/app/polls/polls.showController.js
@@ -9,10 +9,27 @@
 
   function showController ( PollFactory, OptionFactory, $http, $routeParams) {
     var vm = this;
+    vm.pollNum = $routeParams.poll_id
+    console.log(vm.pollNum);
     vm.sum = 0;
 
-    $http.get('http://localhost:3000/api/polls/' + $routeParams.poll_id).then(function(data) {
-      vm.showPoll = data.data.poll;
+    vm.upVote = function(){
+      var ID = vm.chooseVote;
+      var choice = OptionFactory.get({id:ID}, function(){
+      console.log('pre choice', choice.option)
+        choice.option.vote += 1;
+        choice.$update(choice.option);
+        console.log('post choice', choice.option.vote)
+        PollFactory.get({id:vm.pollNum}).$promise.then(function(data){
+          console.log('promise data is ', data.poll)
+          vm.showPoll = data.poll
+        })
+      });
+    };
+
+    PollFactory.get({id: vm.pollNum}).$promise.then(function(data) {
+      console.log('get request ', data.poll.options);
+      vm.showPoll = data.poll;
       vm.pollLabels = [];
       vm.pollData = [];
 
